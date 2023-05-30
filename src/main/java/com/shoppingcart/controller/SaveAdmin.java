3dd23944c3189ce92b8d24a75de608dfff8eca7b
Kunal Dabhane <kunaldabhane20012@gmail.com>
2023-05-31 05:06:12 +0530

    pages added

--- src/main/java/com/shoppingcart/controller/SaveAdmin.java
+++ src/main/java/com/shoppingcart/controller/SaveAdmin.java
@@ -1,16 +1,25 @@
 package com.shoppingcart.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.shoppingcart.dto.Admin;
+import com.shoppingcart.dto.Customer;
+import com.shoppingcart.dto.Merchant;
 
 /**
  * Servlet implementation class SaveAdmin
@@ -18,33 +27,104 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/validate")
 public class SaveAdmin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SaveAdmin() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public SaveAdmin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("kunal");
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction et = em.getTransaction();
-		int id ;
-		String email;
-		String password;
+		response.setContentType("text/html");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String choice = request.getParameter("choice");
+		boolean result = false;
+		PrintWriter pw = response.getWriter();
+		RequestDispatcher rd = request.getRequestDispatcher("Login.html");
+		if(choice==null) {
+			pw.print("<script>alert(\"choose an option for login\")</script>");
+			rd.include(request, response);
+			
+			
+		}
+		if (choice.equals("customer")) {
+			Query query = em.createQuery("select a from Customer a where a.email=?1 and a.password=?2");
+			query.setParameter(1, email);
+			query.setParameter(2, password);
+			List<Customer> customer = query.getResultList();
+			result = customer.size() >=1;
+			if (result) {
+				rd= request.getRequestDispatcher("customeroptions.html");
+			}else {
+				pw.print("<script>alert(\"invalid credentials\")</script>");
+			}
+
+		}
+		if (choice.equals("admin")) {
+			Query query = em.createQuery("select a from Admin a where a.email=?1 and a.password=?2");
+			query.setParameter(1, email);
+			query.setParameter(2, password);
+			List<Admin> admins = query.getResultList();
+			result = admins.size() >=1;
+			
+			if (result) {
+				Admin admin = admins.get(0);
+				HttpSession session   = request.getSession();
+				session.setAttribute("admin", admin);
+				rd= request.getRequestDispatcher( "adminoptions.html");
+			}else {
+				pw.print("<script>alert(\"invalid credentials\")</script>");
+			}
+			
+
+		}
+		if (choice.equals("merchant")) {
+			Query query = em.createQuery("select a from Merchant a where a.email=?1 and a.password=?2");
+			query.setParameter(1, email);
+			query.setParameter(2, password);
+			List<Merchant> merchants = query.getResultList();
+			result = merchants.size() >=1;
+			Merchant merchant =null;
+
+			if (result) {
+
+				merchant= merchants.get(0);
+				rd = request.getRequestDispatcher("merchantoptions.html");
+			}else {
+				pw.print("<script>alert(\"invalid credentials\")</script>");
+				}
+			if (merchant != null && merchant.getStatus().equals("inactive")) {
+				pw.print("<script>alert(\"inactive account\")</script>");
+			}
+			if (merchant != null && merchant.getStatus().equals("blocked")) {
+				pw.print("<script>alert(\"blocked account\")</script>");
+			}
+			
+
+		}
+		
+		rd.include(request, response);
 	}
 
 }
---
+++ src/main/webapp/approvemerchant.jsp
@@ -0,0 +1,67 @@
+<%@page import="org.hibernate.internal.build.AllowSysOut"%>
+<%@page import="java.io.PrintWriter"%>
+<%@page import="com.shoppingcart.dto.Merchant"%>
+<%@page import="java.util.List"%>
+<%@page import="javax.persistence.Query"%>
+<%@page import="javax.persistence.EntityManager"%>
+<%@page import="javax.persistence.Persistence"%>
+<%@page import="javax.persistence.EntityManagerFactory"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+EntityManagerFactory emf = Persistence.createEntityManagerFactory("kunal");
+EntityManager em = emf.createEntityManager();
+
+Query query = em.createQuery("select a from Merchant a where a.status != 'active' ");
+List<Merchant> merchants =  query.getResultList();
+
+
+if(merchants.size()==0){
+	PrintWriter pw = response.getWriter();
+	pw.print("<script>alert(\"no merchants to be approved\")</script>");
+	request.getRequestDispatcher("adminoptions.html").include(request, response);
+	
+}else{
+%>
+
+
+<table id="table-1" border="1px" cellspacing="5px">
+	<thead>
+		<tr>
+			<th>merchants id</th>
+			<th>merchants name</th>
+			<th>email</th>
+			<th>status</th>
+		</tr>
+	</thead>
+	<tbody>
+	<% 
+for(Merchant merchant:merchants){
+	
+	
+	
+	
+%>
+		<tr>
+			<td><%=merchant.getId()%></td>
+			<td><%=merchant.getName()%></td>
+			<td><%=merchant.getEmail()%></td>
+			<td><a href="merchantstatusactive.jsp?id=<%=merchant.getId()%>" title="<%=merchant.getStatus()%>">activate</a></td>
+			
+		</tr>
+		<%
+		
+}} %>
+	</tbody>
+	
+</table>
+
+</body>
+</html>
